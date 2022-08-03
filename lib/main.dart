e4f56dbd11c7902e8bab510cd8b7a287165c5316
Raxan7 <manyerere201@gmail.com>
2022-08-03 20:03:39 +0300

    Decreased the card elevation in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -103,7 +103,7 @@ class _MyHomePageState extends State<MyHomePage> {
   }
   Widget buildRecipeCard(Recipe recipe) {
     return Card(
-      elevation: 4.0,
+      elevation: 2.0,
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(10.0)),
       child: Column(
